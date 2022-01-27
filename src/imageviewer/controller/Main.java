17aa1b78021980afafb549e785bd44047d64533a
G10Mini <G10Mini@DESKTOP-4RFEI7P>
2022-01-27 05:45:36 +0000

    ImageViewer_V1

---
+++ src/imageviewer/controller/Main.java
@@ -0,0 +1,15 @@
+package imageviewer.controller;
+
+import imageviewer.model.Image;
+import imageviewer.persistence.FileImageLoader;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("Images");
+        FileImageLoader imageLoader = new FileImageLoader(file);
+        Image image = imageLoader.load();
+        MainFrame mainFrame = new MainFrame(imageLoader);
+        mainFrame.getImageDisplay().show(image);
+    }
+}
